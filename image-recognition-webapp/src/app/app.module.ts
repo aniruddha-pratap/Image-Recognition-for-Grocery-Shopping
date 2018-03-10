af0738c03d1a8f6a34fa2f39f79c77f276cb953f
aniruddha-pratap <aniruddhapratap.singh@sjsu.edu>
2018-03-09 21:26:49 -0800

    New components

--- image-recognition-webapp/src/app/app.module.ts
+++ image-recognition-webapp/src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes} from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './app-header/app-header.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { CartComponent } from './cart/cart.component';
 
+
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'user', component: UsersComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppHeaderComponent
+    AppHeaderComponent,
+    HomeComponent,
+    UsersComponent,
+    CartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
